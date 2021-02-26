d2d7f66600b64ff60171a05b61d51e7012e169b9
FacundoCampora <79588820+FacundoCampora@users.noreply.github.com>
2021-02-26 17:21:06 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const menuIcon = document.querySelector(".hamburguer-menu");
+const navBar = document.querySelector(".navbar");
+
+
+menuIcon.addEventListener("click", () =>{
+    navBar.classList.toggle("change")
+})
+
+
+
+
+// Slider Slider Slider Slider
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
